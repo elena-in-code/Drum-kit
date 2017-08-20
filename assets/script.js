455fe3655a58fa5b12ab804f2a0f5213a01db723
elena-in-code <30567608+elena-in-code@users.noreply.github.com>
2017-08-20 18:43:42 +0200

    working js
    added js and css, fully functional

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,17 @@
-window.addEventListener('keydown', function(e) {
-	const audio = document.querySelector('audio[data-key=${e.keyCode}]');
-});
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if(!audio) return; //stop the function
+    audio.currentTime = 0; //rewind to the start
+    audio.play();	//playa audio
+    key.classList.add('playing'); //add a class
+}
+
+ function removeTransition(e){
+	if (e.propertyName !== 'transform') return; //skip if it is not a transform
+	this.classList.remove('playing'); // remove the class
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
